df6f16d151d60462a905fd3b5f6620e856a55fbb
hautrna7201 <tranhoanghau7201@gmail.com>
2023-05-26 15:18:19 +0700

    Add: download_data module

--- Utils.py
+++ Utils.py
@@ -1,6 +1,33 @@
 import nltk 
 import re 
 import os
+import gdown
+import sys
+
+def read_file(path):
+  file = open(path, 'r')
+
+  # Read file
+  content = file.read()
+
+  # Close file
+  file.close()
+
+  return content
+
+
+def download_data():
+  source = {
+    'ItemList': read_file('Item List\Data\Source.txt'),
+    'MweList': read_file('Mwe List\Data\Source.txt')
+  }
+
+  # Download ItemList
+  gdown.download_folder(source['ItemList'], output='Item List\Data')
+
+  # Download MweList
+  gdown.download_folder(source['MweList'], output='Mwe List\Data')
+
 
 def tokenizer(data, mwe=[], lang='english'):
   tokens = []
@@ -16,3 +43,6 @@ def clean_data(data):
     result.append(re.sub(r"[,.]", " , ", i.lower())) 
 
   return result
+
+if __name__ == '__main__':
+  globals()[sys.argv[1]]()

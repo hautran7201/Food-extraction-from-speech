196492d1fa1a9a14498030a1234eb8fdce02b597
hautrna7201 <tranhoanghau7201@gmail.com>
2023-05-26 13:49:23 +0700

    Update: Create Module

--- TranscribeText.py
+++ TranscribeText.py
@@ -1,7 +1,16 @@
 import whisper
+import sys
 
 def transcribe_text(path, model='base', language='english'):
     model = whisper.load_model(model)
     result = model.transcribe(path, fp16=False, language=language)
 
     return result
+
+if __name__ == '__main__':
+    path = sys.argv[1] if len(sys.argv) > 1 else None
+    model = sys.argv[2] if len(sys.argv) > 2 else 'base'
+    language = sys.argv[3] if len(sys.argv) > 3 else 'english'
+
+    print('\nSpeech to text:\n')
+    print(transcribe_text(path, model, language)['text'])
--- Utils.py
+++ Utils.py
@@ -2,15 +2,10 @@ import nltk
 import re 
 import os
 
-BASE_DIR = os.getcwd()
-
-nltk.download('punkt',download_dir=os.path.join(BASE_DIR,'env'))
-
 def tokenizer(data, mwe=[], lang='english'):
   tokens = []
   tokenizer = nltk.MWETokenizer(mwe)
   data = clean_data(nltk.sent_tokenize(data))
-  print(data)
   [tokens.append(tokenizer.tokenize(sentence.split())) for sentence in data] 
   return tokens
 
